d6c4b768bae251afa205686c82b65ecefde09d3a
zuo_ji <zuo_ji@worksap.co.jp>
2020-11-17 19:07:29 +0800

    update

--- services/user/src/main/java/playgroud/user/UserApplication.java
+++ services/user/src/main/java/playgroud/user/UserApplication.java
@@ -3,9 +3,11 @@ package playgroud.user;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserApplication {
 
     public static void main(String[] args) {
