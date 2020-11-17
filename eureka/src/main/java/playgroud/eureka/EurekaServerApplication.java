7ea695fdfd43a16cb132d40e7f956b81e6ccdd17
zuo_ji <zuo_ji@worksap.co.jp>
2020-11-17 18:50:50 +0800

    Add eureka server

---
+++ eureka/src/main/java/playgroud/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package playgroud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * refs https://spring.io/guides/gs/service-registration-and-discovery/
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( EurekaServerApplication.class, args );
+    }
+}
--- services/product/src/main/java/playgroup/product/ProductApplication.java
+++ services/product/src/main/java/playgroup/product/ProductApplication.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class ProductApplication {
 
     public static void main(String[] args) {
