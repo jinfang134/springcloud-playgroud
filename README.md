ba72a18953c6f8709cd3bfca755823bbf4080891
zuo_ji <zuo_ji@worksap.co.jp>
2020-11-18 16:25:04 +0800

    Add config server

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # springcloud-playgroud
 
 
+## endpoint
+- [admin server](http://localhost:8000)
+- [eureka](http://localhost:8761)
+- [config server](http://localhost:8888)
+- Services
+    - [user service](http://localhost:8002)
+    - [product](http://localhost:8001)
 
 ## refs
 - [Service Registration and Discovery](https://spring.io/guides/gs/service-registration-and-discovery/)
+- [Centralized Configuration](https://spring.io/guides/gs/centralized-configuration/)
--- services/product/src/main/java/playgroup/product/ProductApplication.java
+++ services/product/src/main/java/playgroup/product/ProductApplication.java
@@ -1,13 +1,21 @@
 package playgroup.product;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Configuration
 @EnableAutoConfiguration
@@ -23,6 +31,23 @@ public class ProductApplication {
     @GetMapping("/api/product")
     public Product getProduct() {
         return new Product(12L, "test name");
+    }
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @GetMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
     }
 }
 
