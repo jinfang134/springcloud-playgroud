8b86caa9a719fd77acee562e6e60039b64a54558
zuo_ji <zuo_ji@worksap.co.jp>
2020-11-20 17:00:26 +0800

    extract controller

--- services/product/src/main/java/playgroup/product/ProductApplication.java
+++ services/product/src/main/java/playgroup/product/ProductApplication.java
@@ -1,29 +1,12 @@
 package playgroup.product;
 
-import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import playground.entity.Product;
 
-import java.util.List;
-
-@Configuration
-@EnableAutoConfiguration
 @SpringBootApplication
-@RestController
 @EnableEurekaClient
 public class ProductApplication {
 
@@ -33,26 +16,5 @@ public class ProductApplication {
         SpringApplication.run(ProductApplication.class, args);
     }
 
-    @GetMapping("/api/product")
-    public Product getProduct() {
-        log.info("call get product");
-        return new Product(12L, "test name");
-    }
 
-    @Value("${message:Hello default}")
-    private String message;
-
-    @GetMapping("/message")
-    String getMessage() {
-        return this.message;
-    }
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
-    }
 }
---
+++ services/product/src/main/java/playgroup/product/ProductController.java
@@ -0,0 +1,43 @@
+package playgroup.product;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import playground.entity.Product;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    private Logger log = LoggerFactory.getLogger(ProductController.class);
+
+    @GetMapping("/api/product")
+    public Product getProduct() {
+        log.info("call get product");
+        return new Product(12L, "test name");
+    }
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @GetMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
