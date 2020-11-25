5bd393bbc5c14837b57688a182422e16951f76a0
zuo_ji <zuo_ji@worksap.co.jp>
2020-11-25 12:57:00 +0800

    Add request header for open feign client.

---
+++ core/entity/src/main/java/playground/annotation/MyRequestInterceptor.java
@@ -0,0 +1,31 @@
+package playground.annotation;
+
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.support.RequestContext;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class MyRequestInterceptor implements RequestInterceptor {
+    public static final String X_CLIENT_TOKEN_USER = "x-client-token-user";
+
+    @Override
+    public void apply(RequestTemplate template) {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        //获取请求对象
+        HttpServletRequest request = requestAttributes.getRequest();
+        Map<String, Collection<String>> headers = new HashMap<>();
+        headers.put(X_CLIENT_TOKEN_USER, Collections.singletonList(request.getHeader(X_CLIENT_TOKEN_USER)));
+        template.headers(headers);
+    }
+
+}
