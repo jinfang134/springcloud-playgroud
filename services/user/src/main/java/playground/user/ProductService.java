c1a5d4cd4d9e9134d3b0bee585263153ae408de6
zuo_ji <zuo_ji@worksap.co.jp>
2020-11-25 18:41:23 +0800

    update

--- services/user/src/main/java/playground/user/ProductService.java
+++ services/user/src/main/java/playground/user/ProductService.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import playground.entity.Product;
 
-@FeignClient(value = "product", fallback = ProductFallback.class)
+//@FeignClient(value = "product",url="http://localhost:8001", fallback = ProductFallback.class)
+@FeignClient(value = "product",url="", fallback = ProductFallback.class)
 public interface ProductService {
 
     @GetMapping(value = "/api/product")
