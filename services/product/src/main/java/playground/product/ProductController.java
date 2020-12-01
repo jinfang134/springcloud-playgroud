39f9238132f5c732015a4f1538a23ef57ea379c5
zuo_ji <zuo_ji@worksap.co.jp>
2020-12-01 11:34:02 +0800

    update

--- services/product/src/main/java/playground/product/ProductController.java
+++ services/product/src/main/java/playground/product/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import playground.entity.Product;
 import java.util.List;
 
 @RestController
+@RefreshScope
 public class ProductController {
     private Logger log = LoggerFactory.getLogger(ProductController.class);
 
